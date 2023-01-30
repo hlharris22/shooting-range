d0d71a46ea41734783f0fa3664cf7a49fe15b87e
Hunter Harris <hlharris22@gmail.com>
2023-01-30 16:07:35 -0500

    Added shooting functionality

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ import random
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+pygame.mouse.set_visible(False)
 
-wood_bg = pygame.image.load("shooting+range+assets/shooting range assets/Wood_BG.png")
-land_bg = pygame.image.load("shooting+range+assets/shooting range assets/Land_BG.png")
-water_bg = pygame.image.load("shooting+range+assets/shooting range assets/Water_BG.png")
-cloud_1 = pygame.image.load("shooting+range+assets/shooting range assets/Cloud1.png")
-cloud_2 = pygame.image.load("shooting+range+assets/shooting range assets/Cloud2.png")
-crosshair = pygame.image.load("shooting+range+assets/shooting range assets/crosshair.png")
-duck_surface = pygame.image.load("shooting+range+assets/shooting range assets/duck.png")
+wood_bg = pygame.image.load("shooting+range+assets/images/Wood_BG.png")
+land_bg = pygame.image.load("shooting+range+assets/images/Land_BG.png")
+water_bg = pygame.image.load("shooting+range+assets/images/Water_BG.png")
+cloud_1 = pygame.image.load("shooting+range+assets/images/Cloud1.png")
+cloud_2 = pygame.image.load("shooting+range+assets/images/Cloud2.png")
+crosshair = pygame.image.load("shooting+range+assets/images/crosshair.png")
+duck_surface = pygame.image.load("shooting+range+assets/images/duck.png")
+gun_shot = pygame.mixer.Sound("shooting+range+assets/sounds/402012__eardeer__gunshot__high_5.wav")
+
+game_font = pygame.font.Font(None, 60)
+text_surface = game_font.render("You Won!", True, (255, 255, 255))
+score = 0
 
 land_position_y = 560
 land_speed = 1
@@ -22,6 +28,7 @@ water_position_y = 640
 water_speed = 1.25
 
 crosshair_rect = crosshair.get_rect(center=(640, 360))
+text_surface_rect = text_surface.get_rect(center=(640, 360))
 
 duck_list = []
 for duck in range(0, 20):
@@ -37,6 +44,11 @@ while True:
             sys.exit()
         if event.type == pygame.MOUSEMOTION:
             crosshair_rect = crosshair.get_rect(center=event.pos)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.mixer.Sound.play(gun_shot)
+            for duck in duck_list:
+                if duck.collidepoint(event.pos):
+                    duck_list.remove(duck)
     screen.blit(wood_bg, (0, 0))
 
     land_position_y -= land_speed
@@ -45,6 +57,8 @@ while True:
     screen.blit(land_bg, (0, land_position_y))
     for duck_rect in duck_list:
         screen.blit(duck_surface, duck_rect)
+    if not duck_list:
+        screen.blit(text_surface, text_surface_rect)
     water_position_y -= water_speed
     if water_position_y <= 575 or water_position_y >= 660:
         water_speed *= -1
@@ -58,5 +72,6 @@ while True:
     screen.blit(cloud_2, (900, 55))
     screen.blit(cloud_2, (100, 10))
     screen.blit(cloud_2, (500, 100))
+
     pygame.display.update()
     clock.tick(120)
